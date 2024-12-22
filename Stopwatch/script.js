470ce5fc45deaea7e69b586845e98a00bab0cfb1
Farhan Ali Reza <farhanalireza50@gmail.com>
2024-12-22 18:08:41 +0530

    minor0.2.1

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -32,6 +32,8 @@ function reset() {
     isRunning = false;
     elapsedTime = 0;
     display.textContent = '00:00:00.000';
+    controls.innerHTML = `<button id="start" onclick="start()"><svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#e8eaed"><path xmlns="http://www.w3.org/2000/svg" d="m367-276 317-204-317-204v408ZM480-34q-92.64 0-174.47-34.6-81.82-34.61-142.07-94.86T68.6-305.53Q34-387.36 34-480q0-92.9 34.66-174.45 34.67-81.55 95.18-141.94 60.51-60.39 142.07-95Q387.48-926 480-926q92.89 0 174.48 34.59 81.59 34.6 141.96 94.97 60.37 60.37 94.97 141.99Q926-572.83 926-479.92q0 92.92-34.61 174.25-34.61 81.32-95 141.83Q736-103.33 654.45-68.66 572.9-34 480-34Z"/></svg></button>
+            <button id="reset" onclick="reset()"><svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#e8eaed"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"/></svg></button>`;
 }
 
 function update() {
