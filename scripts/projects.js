f8a2d224f3a7191ef260df739caad8afe047c675
Vincent <farhanalireza50@gmail.com>
2025-02-11 20:14:38 +0530

    minor_changes 0.6.0.1

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,3 +1,5 @@
 function loadProject(projectPath) {
     document.getElementById('project-frame').src = projectPath + "/index.html";
 }
+
+document.addEventListener("DOMContentLoaded", () => loadProject('../about'));
