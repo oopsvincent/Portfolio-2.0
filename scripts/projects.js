e5ebaa6fd1bae9e2b8a8df2d34847e0cdd660d0f
Vincent <farhanalireza50@gmail.com>
2025-02-12 18:11:24 +0530

    minor_changes 0.6.0.3

--- scripts/projects.js
+++ scripts/projects.js
@@ -3,3 +3,19 @@ function loadProject(projectPath) {
 }
 
 document.addEventListener("DOMContentLoaded", () => loadProject('../about'));
+
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    // navbar.style.transform = 'translateX(0px)';
+});
+
+
+// if (document.) {
+    
+// }
+// navbar.addEventListener("click", () => {
+//     navbar.classList.toggle("active");
+// })
