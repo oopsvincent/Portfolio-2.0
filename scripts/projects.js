1cb4c4f9548430b1bde9c903e93095c0bb2c9f91
Vincent Brown <farhanalireza50@gmail.com>
2025-03-24 10:02:19 +0530

    new feature add

--- scripts/projects.js
+++ scripts/projects.js
@@ -3,6 +3,7 @@ function loadProject(projectPath) {
     // document.getElementById('fullPage').href = projectPath;
 }
 
+
 document.addEventListener("DOMContentLoaded", () => loadProject('../about'));
 
 const menuToggle = document.getElementById("menu-toggle");
@@ -23,8 +24,8 @@ if (window.innerWidth <= 1200) {
 const iframe = document.getElementById('project-frame');
 const button = document.getElementById('fullscreen-button');
 
-button.addEventListener('click', () => {
-  if (iframe.requestFullscreen) {
+function requestFullScreen(){
+    if (iframe.requestFullscreen) {
     iframe.requestFullscreen();
   } else if (iframe.mozRequestFullScreen) {
     iframe.mozRequestFullScreen();
@@ -32,5 +33,12 @@ button.addEventListener('click', () => {
     iframe.webkitRequestFullscreen();
   } else if (iframe.msRequestFullscreen) {
     iframe.msRequestFullscreen();
+  }
+}
+
+button.addEventListener('click', requestFullScreen);
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'f' || event.key === 'F') {
+    requestFullScreen();
   }
 });
